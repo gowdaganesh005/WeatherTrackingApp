754073395227dad42c96bcbec2c1368b06e1d41f
gowdaganesh005 <gowdaganesh0511@gmail.com>
2024-02-03 21:33:51 +0530

    adding struct

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
+type apiConfigData struct {
+	OpenWeatherMapApiKey string `json:"OpenWeatherMapApiKey"`
+}
+type weatherdata struct {
+	name string
+	Main struct {
+		Kelvin float64
+	}
+}
+
 func main() {
 
 }
