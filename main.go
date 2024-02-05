0c989259d804c50e49dbd4f0d087cc7524f16157
gowdaganesh005 <gowdaganesh0511@gmail.com>
2024-02-05 22:29:53 +0530

    Enabled Geocoder feature and no more to remember the lat and lon

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
 
@@ -18,23 +19,45 @@ type weatherdata struct {
 	} `json:"main"`
 	Name string `json:"name"`
 }
+type locadata struct {
+	Lat float64 `json:"lat"`
+	Lon float64 `json:"lon"`
+}
 
 func main() {
 
 	godotenv.Load()
-	var lat, lon float64
-	fmt.Printf("Enter the lat and lon :")
-	fmt.Scanf("%v %v", &lat, &lon)
+
+	var city string
+	fmt.Printf("Enter the city :")
+	fmt.Scanf("%v ", &city)
 
 	client := &http.Client{}
 
 	ApiKey := os.Getenv("OpenWeatherMapApiKey")
-
-	req, err := http.NewRequest("GET", fmt.Sprintf("https://api.openweathermap.org/data/2.5/weather?lat=%v&lon=%v&appid=%v", lat, lon, ApiKey), nil)
+	locreq, err := http.NewRequest("GET", fmt.Sprintf("http://api.openweathermap.org/geo/1.0/direct?q=%v&limit=2&appid=%v", city, ApiKey), nil)
+	if err != nil {
+		log.Println("Error in getting location", err)
+	}
+	locres, err := client.Do(locreq)
+	if err != nil {
+		fmt.Println("Error in response:", err)
+		return
+	}
+	defer locres.Body.Close()
+	location := []locadata{}
+	err = json.NewDecoder(locres.Body).Decode(&location)
 	if err != nil {
 		fmt.Println("Error creating request:", err)
 		return
 	}
+
+	req, err := http.NewRequest("GET", fmt.Sprintf("https://api.openweathermap.org/data/2.5/weather?lat=%v&lon=%v&appid=%v", location[0].Lat, location[0].Lon, ApiKey), nil)
+	if err != nil {
+		fmt.Println("Error creating request:", err)
+		return
+	}
+
 	res, err := client.Do(req)
 	if err != nil {
 		fmt.Println("Error creating request:", err)
@@ -48,6 +71,6 @@ func main() {
 		fmt.Println("Error creating request:", err)
 		return
 	}
-	fmt.Println(forecast)
+	fmt.Println(forecast.Main.Kelvin)
 
 }
